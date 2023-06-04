7d76130d58e9519605985b1ff920433057facb63
alicimsamil <alicimsamil@gmail.com>
2023-06-04 10:07:41 +0300

    edit data sources

--- app/src/main/java/com/alicimsamil/harmonyhub/data/local/source/LocalDataSource.kt
+++ app/src/main/java/com/alicimsamil/harmonyhub/data/local/source/LocalDataSource.kt
@@ -2,38 +2,32 @@ package com.alicimsamil.harmonyhub.data.local.source
 
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
-import androidx.paging.map
+import androidx.paging.PagingData
 import com.alicimsamil.harmonyhub.core.common.model.TrackDBModel
 import com.alicimsamil.harmonyhub.data.local.db.HarmonyHubDAO
 import com.alicimsamil.harmonyhub.data.local.util.BaseLocalDataSource
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class LocalDataSource @Inject constructor(private val harmonyDao: HarmonyHubDAO) :
     BaseLocalDataSource() {
-    suspend fun addTrack(trackModel: TrackDBModel) {
+    suspend fun addTrack(trackModel: TrackDBModel) =
         performDatabaseOperation {
             harmonyDao.addTrack(trackModel)
         }
-    }
 
-    suspend fun deleteTrack(trackModel: TrackDBModel) {
+    suspend fun deleteTrack(trackModel: TrackDBModel) =
         performDatabaseOperation {
             harmonyDao.deleteTrack(trackModel)
         }
+
+    fun getLocalTracksWithPaging() : Flow<PagingData<TrackDBModel>> {
+        return Pager( config = PagingConfig(pageSize = 20) ){ harmonyDao.getAllTracks() }.flow
     }
 
-    fun getLocalTracksWithPaging() = run {
-        Pager(
-            config = PagingConfig(pageSize = 20)
-        ){
-            harmonyDao.getAllTracks()
-        }.flow.map { it -> it.map { it.copy(artistId = 5) } }
-    }
-
-    suspend fun getTrackById(id: Int) {
+    suspend fun getTrackById(id: Int) =
         performDatabaseOperation {
             harmonyDao.getTrackById(id)
         }
-    }
+
 }
--- app/src/main/java/com/alicimsamil/harmonyhub/data/remote/source/RemoteDataSource.kt
+++ app/src/main/java/com/alicimsamil/harmonyhub/data/remote/source/RemoteDataSource.kt
@@ -1,14 +1,7 @@
 package com.alicimsamil.harmonyhub.data.remote.source
 
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
-import androidx.paging.PagingData
-import androidx.paging.map
-import com.alicimsamil.harmonyhub.core.common.model.TrackModel
 import com.alicimsamil.harmonyhub.data.remote.service.HarmonyHubService
 import com.alicimsamil.harmonyhub.data.remote.util.BaseRemoteDataSource
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class RemoteDataSource @Inject constructor(
@@ -17,17 +10,10 @@ class RemoteDataSource @Inject constructor(
 ) :
     BaseRemoteDataSource() {
 
-    suspend fun getTracks(term: String) {
+    suspend fun getTracks(term: String) =
         performApiCall {
             harmonyHubService.getTracks(term)
         }
-    }
 
-    fun getRemoteTracksWithPaging() : Flow<PagingData<TrackModel>> {
-        return Pager(
-            config = PagingConfig(pageSize = 20)
-        ){
-            tracksPagingSource
-        }.flow.map { it -> it.map { it.copy(artistId = 5) } }
-    }
+
 }
