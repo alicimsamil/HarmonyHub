3cf976da62a3943fcf3b8d5dc079ccb694029b08
alicimsamil <alicimsamil@gmail.com>
2023-06-04 04:50:36 +0300

    add paging source to remoteDataSource

--- app/src/main/java/com/alicimsamil/harmonyhub/data/local/source/LocalDataSource.kt
+++ app/src/main/java/com/alicimsamil/harmonyhub/data/local/source/LocalDataSource.kt
@@ -1,8 +1,12 @@
 package com.alicimsamil.harmonyhub.data.local.source
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.map
 import com.alicimsamil.harmonyhub.core.common.model.TrackDBModel
 import com.alicimsamil.harmonyhub.data.local.db.HarmonyHubDAO
 import com.alicimsamil.harmonyhub.data.local.util.BaseLocalDataSource
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class LocalDataSource @Inject constructor(private val harmonyDao: HarmonyHubDAO) :
@@ -19,7 +23,13 @@ class LocalDataSource @Inject constructor(private val harmonyDao: HarmonyHubDAO)
         }
     }
 
-    fun getAllTracks() = harmonyDao.getAllTracks()
+    fun getLocalTracksWithPaging() = run {
+        Pager(
+            config = PagingConfig(pageSize = 20)
+        ){
+            harmonyDao.getAllTracks()
+        }.flow.map { it -> it.map { it.copy(artistId = 5) } }
+    }
 
     suspend fun getTrackById(id: Int) {
         performDatabaseOperation {
