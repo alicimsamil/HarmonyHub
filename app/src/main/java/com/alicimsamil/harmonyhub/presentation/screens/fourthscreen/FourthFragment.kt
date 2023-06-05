ff4152e17d0d870eb5235dbc830d3f72abca3d4f
alicimsamil <alicimsamil@gmail.com>
2023-06-05 13:17:52 +0300

    edit fourth item

--- app/src/main/java/com/alicimsamil/harmonyhub/presentation/screens/fourthscreen/FourthFragment.kt
+++ app/src/main/java/com/alicimsamil/harmonyhub/presentation/screens/fourthscreen/FourthFragment.kt
@@ -1,10 +1,13 @@
 package com.alicimsamil.harmonyhub.presentation.screens.fourthscreen
 
+import android.app.AlertDialog
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import com.alicimsamil.harmonyhub.R
 import com.alicimsamil.harmonyhub.core.presentation.BaseFragment
+import com.alicimsamil.harmonyhub.core.presentation.InteractWithItem
 import com.alicimsamil.harmonyhub.databinding.FragmentFourthBinding
 import com.alicimsamil.harmonyhub.core.presentation.TracksAdapter
 import dagger.hilt.android.AndroidEntryPoint
@@ -12,17 +15,19 @@ import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
-class FourthFragment : BaseFragment<FragmentFourthBinding, FourthViewModel>(FragmentFourthBinding::inflate) {
+class FourthFragment :
+    BaseFragment<FragmentFourthBinding, FourthViewModel>(FragmentFourthBinding::inflate) {
     override val viewModel by viewModels<FourthViewModel>()
     override var state = FourthScreenUiState()
 
-    private lateinit var pagingAdapter : TracksAdapter
+    private lateinit var pagingAdapter: TracksAdapter
 
     override fun initialize() {
         super.initialize()
         viewModel.onEvent(FourthScreenUiEvent.GetPagingData)
         setRecyclerAdapter()
         observeUiState()
+        showDeletedSuccessPopUp()
     }
 
     private fun observeUiState() {
@@ -30,19 +35,39 @@ class FourthFragment : BaseFragment<FragmentFourthBinding, FourthViewModel>(Frag
             repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.state.collectLatest {
                     state = it
-                    handleFailure()
-                    handleLoading()
-                    state.pagingData?.collectLatest {data->
+                    state.pagingData?.collectLatest { data ->
                         pagingAdapter.submitData(data)
                     }
+                    handleFailure()
+                    handleLoading()
                 }
             }
         }
     }
 
     private fun setRecyclerAdapter() {
-        pagingAdapter = TracksAdapter(TracksAdapter.NoteComparator)
+        pagingAdapter = TracksAdapter(TracksAdapter.NoteComparator, object : InteractWithItem {
+            override fun delete(id: Int) {
+                viewModel.onEvent(FourthScreenUiEvent.DeleteTrack(id))
+            }
+        })
         binding.rvFourth.adapter = pagingAdapter
     }
 
+    private fun showDeletedSuccessPopUp() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.eventFlow.collectLatest {
+                    if (it) {
+                        AlertDialog.Builder(context)
+                            .setTitle(getString(R.string.delete))
+                            .setMessage(getString(R.string.delete_success))
+                            .create()
+                            .show()
+                        viewModel.eventFlow.emit(false)
+                    }
+                }
+            }
+        }
+    }
 }
--- app/src/main/java/com/alicimsamil/harmonyhub/presentation/screens/fourthscreen/FourthScreenUiEvent.kt
+++ app/src/main/java/com/alicimsamil/harmonyhub/presentation/screens/fourthscreen/FourthScreenUiEvent.kt
@@ -1,9 +1,8 @@
 package com.alicimsamil.harmonyhub.presentation.screens.fourthscreen
 
-import com.alicimsamil.harmonyhub.core.common.model.TrackDBModel
 import com.alicimsamil.harmonyhub.core.presentation.UiEvent
 
 sealed class FourthScreenUiEvent() : UiEvent {
     object GetPagingData : FourthScreenUiEvent()
-    data class DeleteTrack(val listDBModel: TrackDBModel) : FourthScreenUiEvent()
+    data class DeleteTrack(val id: Int) : FourthScreenUiEvent()
 }
