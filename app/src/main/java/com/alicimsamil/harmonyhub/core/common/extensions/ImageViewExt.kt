574115758fceb654aa6ae43b4e6512e77c671041
alicimsamil <alicimsamil@gmail.com>
2023-06-05 11:40:20 +0300

    create imageview ext

---
+++ app/src/main/java/com/alicimsamil/harmonyhub/core/common/extensions/ImageViewExt.kt
@@ -0,0 +1,16 @@
+package com.alicimsamil.harmonyhub.core.common.extensions
+
+import android.content.Context
+import android.widget.ImageView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import com.bumptech.glide.Glide
+
+
+fun ImageView.loadUrl(url: String?, context: Context) {
+    val circularProgressDrawable = CircularProgressDrawable(context)
+    circularProgressDrawable.strokeWidth = 5f
+    circularProgressDrawable.centerRadius = 30f
+    circularProgressDrawable.start()
+
+    Glide.with(this).load(url).placeholder(circularProgressDrawable).into(this)
+}
